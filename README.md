bc9ef3c5b5d6d1a9da5fe9a7d11bc75689f4d934
Arnd Rensink <arend.rensink@utwente.nl>
2017-01-06 17:04:15 +0100

    Initial website (minus bulky stuff)

--- README.md
+++
@@ -1,1 +0,0 @@
-# rensink.github.io
---
+++ editorships.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <style type="text/css" media="screen">
+            h1 { text-align: center; margin-top: 40px }
+            h2 { text-align: center }
+            .source {
+                background-color: #d0e0d0;
+                border: 1px solid #00a000;
+            }
+            .citation_group { margin-bottom: 50px }
+            .citation .field_creators, .citation .field_editors { font-size: 66% }
+            .citation .person_name { font-size: 150% }
+            .citation {
+                border: 1px solid red;
+            }
+            .citation, .source {
+                padding: 5px;
+                margin-right: 30px;
+                margin-left: 30px;
+                margin-bottom: 10px;
+            }
+        </style>
+    <title>Publications of Arend Rensink</title>
+    <link rel="shortcut icon" href="ut-icon.ico">
+</head>
+<body>
+<?php readfile("http://eprints.eemcs.utwente.nl/view/editor/Rensink,_A.include"); ?>
+</body>
+</html>
